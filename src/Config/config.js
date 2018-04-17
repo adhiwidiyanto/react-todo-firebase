2f48d881a87d0b05792291629cf457b2096ade3c
mrkentung <widiyanto.adhi@gmail.com>
2018-04-17 11:00:32 +0700

    Initial commit

---
+++ src/Config/config.js
@@ -0,0 +1,8 @@
+export const DB_CONFIG = {
+    apiKey: "AIzaSyCIIpRQ01t82I4DyWavJY4cONfPjVMc05k",
+    authDomain: "notes-app-6e07f.firebaseapp.com",
+    databaseURL: "https://notes-app-6e07f.firebaseio.com",
+    projectId: "notes-app-6e07f",
+    storageBucket: "notes-app-6e07f.appspot.com",
+    messagingSenderId: "894470196079"
+};
